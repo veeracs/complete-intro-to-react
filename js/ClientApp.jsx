cbe6fadd7ae3d3ac289ef58559f2de7c8f75837a
veeracs <veeracs@gmail.com>
2018-07-24 09:08:48 -0400

    setup webpack bundle

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const myFirstComponent = () => React.createElement('div', null, React.createElement('h1', null, 'This is my first component...'));
+const myFirstComponent = () => React.createElement('div', null, React.createElement('h1', null, 'This is mmy first component...'));
 
 ReactDOM.render(React.createElement(myFirstComponent), document.getElementById('app'));
